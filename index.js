5be540fb2e6df058993bd563468b47e25d93d0c2
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2022-06-29 18:44:28 +0600

    implement=post&put-request

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+const Joi = require('joi');
+
 const express = require('express');
 const app = express();
+
+
+app.use(express.json())
+const courses = [
+    {id:1 ,name: 'Software Design' },
+    {id:2 ,name: 'Software Metrics' },
+    {id:3 ,name: 'Software Security' }
+
+]
 
 app.get('/', (req,res)=> {
     res.send('Welcome to Facebook');
@@ -10,11 +21,50 @@ app.get('/friends', (req,res)=> {
     
 });
 
-app.get('/api/courses', (req,res)=> {
-    res.send([1,2,3,4,5,6,7,8]);
+app.get('/posts', (req,res)=> {
+    res.send(['Al hamdulillah 632 problem solved in codeforeces','Started School at University of Dhaka','Office Secretary at Khilkhet Pathagar','Tanguar Haor Tour','Kurmitolian Reunion 2020']);
     
 });
 
+
+app.get('/api/courses', (req,res)=> {
+    res.send(courses);
+    
+});
+
+app.get('/api/courses/:id', (req,res)=> {
+    const course = courses.find(c=>c.id===parseInt(req.params.id));
+
+    if(!course) res.status(404).send('This course is not available');
+    res.send(course);
+    
+});
+
+app.post('/api/courses', (req,res)=>{
+
+   
+  
+    if(!req.body.name || req.body.name.length<3){
+        res.status(400).send('Name must be greater than 3 characters');
+        return;
+    }
+const course = {
+    id: courses.length+1,
+    name: req.body.name,
+}
+courses.push(course);
+res.send(course);
+
+});
+
+app.put('/api/courses/:id', (req,res)=>{
+    const course = courses.find(c=>c.id===parseInt(req.params.id));
+
+    if(!course) res.status(404).send('This course is not available');
+
+    course.name = req.body.name;
+    res.send(courses)
+})
 app.get('/api/posts/:year/:month', (req,res)=> {
     res.send(req.params);
     
@@ -22,10 +72,6 @@ app.get('/api/posts/:year/:month', (req,res)=> {
 
 app.get('/api/members/:year/:month', (req,res)=> {
     res.send(req.query);
-    
-});
-app.get('/posts', (req,res)=> {
-    res.send(['Al hamdulillah 632 problem solved in codeforeces','Started School at University of Dhaka','Office Secretary at Khilkhet Pathagar','Tanguar Haor Tour','Kurmitolian Reunion 2020']);
     
 });
 
