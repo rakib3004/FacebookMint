40d748cbfa89d41a88b4a8b10799026f8781c804
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2022-06-29 16:52:08 +0600

    add=post&frineds

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
 
-app.length('/', (res,req)=> {
+app.get('/', (req,res)=> {
     res.send('Welcome to Facebook');
 });
 
-app.length('/api/courses', (res,req)=> {
-    res.send([1,2,3]);
+app.get('/friends', (req,res)=> {
+    res.send(['Muktar','Siam','Mushfiq','Mohsin','Ashraf','Tasmia','Mustahid','Khairul','Sakib','Fahad','Kazi','Jafar']);
+    
+});
+
+app.get('/posts', (req,res)=> {
+    res.send(['Al hamdulillah 632 problem solved in codeforeces','Started School at University of Dhaka','Office Secretary at Khilkhet Pathagar','Tanguar Haor Tour','Kurmitolian Reunion 2020']);
     
 });
 
