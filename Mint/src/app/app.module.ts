6e0600b6d7327d2a973b6aee59690c0501e3b94d
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2022-07-03 01:33:11 +0600

    add login sign up and status component

--- Mint/src/app/app.module.ts
+++ Mint/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { StatusComponent } from './status/status.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    StatusComponent,
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
