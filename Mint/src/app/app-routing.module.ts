5037addada616314fa40fe5a62010aff0ea198c7
rakib3004 <bsse1129@iit.du.ac.bd>
2022-07-18 15:46:32 +0600

    add new component status routing

--- Mint/src/app/app-routing.module.ts
+++ Mint/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { StatusComponent } from './status/status.component';
+import { StoryComponent } from './story/story.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'status', component: StatusComponent },
+  { path: 'story', component: StoryComponent },
 
+
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
