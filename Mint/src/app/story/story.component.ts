daf46f5293d93101d22a073a6b17765ef1f33585
rakib3004 <bsse1129@iit.du.ac.bd>
2022-07-21 11:21:44 +0600

    file upload

--- Mint/src/app/story/story.component.ts
+++ Mint/src/app/story/story.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FileUploadService } from '../Services/file-upload.service';
 
 @Component({
   selector: 'app-story',
@@ -7,13 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StoryComponent implements OnInit {
 
-  constructor() { }
+  shortLink: string = "";
+  loading: boolean = false; // Flag variable
+  file: File = null; // Variable to store file
 
-  ngOnInit(): void {
-  }
-
-  addYourStory(){
-    alert("Owww Story!")
-  }
+    // Inject service 
+    constructor(private fileUploadService: FileUploadService) { }
+  
+    ngOnInit(): void {
+    }
+  
+    // On file Select
+    onChange(event) {
+        this.file = event.target.files[0];
+    }
+  
+    // OnClick of button Upload
+    onUpload() {
+        this.loading = !this.loading;
+        console.log(this.file);
+        this.fileUploadService.upload(this.file).subscribe(
+            (event: any) => {
+                if (typeof (event) === 'object') {
+  
+                    // Short link via api response
+                    this.shortLink = event.link;
+  
+                    this.loading = false; // Flag variable 
+                }
+            }
+        );
+    }
 
 }
