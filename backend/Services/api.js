445d44800d732d5150ae26941a1f6eed126665ef
rakib3004 <bsse1129@iit.du.ac.bd>
2022-07-21 19:44:00 +0600

    back propagate

--- backend/Services/api.js
+++ backend/Services/api.js
@@ -6,8 +6,27 @@ const app = express();
 app.use(express.json())
 
 app.post('/status', (req,result)=>{
-
+console.log(req.body)
 });
+
+
+const mongoose = require('mongoose');
+
+var cors = require('cors');
+app.use(cors({
+    origin:"http://localhost:4200"
+}))
+
+mongoose.connect('mongodb://localhost:27017/Mint',
+{
+    useNewUrlParser : true
+}
+,()=>{
+    console.log('database connected')
+});
+
+
+
 
 
 const port = process.env.PORT || 3000;
